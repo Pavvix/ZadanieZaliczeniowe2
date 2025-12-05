1621abca51c2c3c3fde3ce43903e039b9d859ae7
Antonio <antekkijakxd@gmail.com>
2025-12-05 13:47:35 +0100

    Generator hasla jeszcze nie gotowy ide robic spinnera bo zaraz oszaleje

--- app/src/main/java/com/example/zadaniezaliczeniowe/MainActivity.java
+++ app/src/main/java/com/example/zadaniezaliczeniowe/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.zadaniezaliczeniowe;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,9 +29,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         EditText firstName = findViewById(R.id.firstName);
         EditText lastName = findViewById(R.id.lastName);
         EditText characterAmount = findViewById(R.id.characterAmount);
+
 
         CheckBox check1 = findViewById(R.id.check1);
         CheckBox check2 = findViewById(R.id.check2);
@@ -39,5 +46,48 @@ public class MainActivity extends AppCompatActivity {
         Spinner dropdownMenu = findViewById(R.id.dropdownMenu);
 
         String generatedPassword = "";
+
+        String lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+        String upperCaseChars = lowerCaseChars.toUpperCase();
+        String numbers = "1234567890";
+        String specialChars = "!@#$%^&*()_+-=.";
+
+//        GENEROWANIE HASLA
+
+        generateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String passwordLength = characterAmount.getText().toString();
+                int finalPasswordLength = Integer.parseInt(passwordLength);
+
+                if(finalPasswordLength == 0){
+                    Toast.makeText(MainActivity.this, "Wybierz dlugość hasła!", Toast.LENGTH_SHORT);
+                }
+                else{
+
+                String possibleChars = lowerCaseChars;
+
+                if(check1.isChecked()){
+                    possibleChars += upperCaseChars;
+                }
+                if(check2.isChecked()){
+                    possibleChars += numbers;
+                }
+                if(check3.isChecked()){
+                    possibleChars += specialChars;
+                }
+
+                    Random random = new Random();
+
+                    for (int i = 0; i < finalPasswordLength; i++) {
+
+                    }
+
+
+            }
+            }
+        });
+
+
     }
 }
